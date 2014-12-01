8fa03ae8ce9318f0b35b1a0d8e0a788f73127823
Angela McKenzie <angelamckenzie@Angelas-MacBook-Pro.local>
2014-12-01 14:03:08 -0500

    Commit

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $feed->handle_content_type();
 // Begin our XHTML markup
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+<!DOCTYPE html PUBLIC "-//W3C/TD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">	
 <head>
